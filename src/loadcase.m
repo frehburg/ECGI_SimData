d4b3fbf99fd37e36357acb5b941b42bbfc05ac0d
Filip Rehburg <fil.rehburg@gmail.com>
2023-02-18 22:35:22 +0100

    Exports all necessary information to csv
    Not sure if right and left blood cavity means right and left half of the heart. Otherwise the triangulation for the atria and ventricles are missing. Can maybe use the conversion matrices in the worst case

--- src/loadcase.m
+++ src/loadcase.m
@@ -11,6 +11,7 @@ mkdir('ECGI_SimData\export', name)
 
 % file names
 ecg_simulation_filename = 'ecgs\';
+model_file_name = 'model\';
 ecg_64_filename = 'BSM_(nijmegen_64).refECG';
 lead_locations_filename = 'BSM_(nijmegen_64).elec';
 ventricular_beats_filename = 'ventricular_beats';
@@ -25,26 +26,37 @@ export_filename = '\export\';
 % columns: time, rows: electrodes
 ecg_64 = loadmat(append(path,'\',ecg_simulation_filename,ecg_64_filename));
 writematrix(ecg_64,append('ECGI_SimData',export_filename,name,'\ecg_64.csv'));
-disp(ecg_64)
 
 % load in the locations for each of the electrodes
 lead_locations = loadmat(append(path,'\',ecg_simulation_filename,'\',lead_locations_filename));
 writematrix(lead_locations,append('ECGI_SimData',export_filename,name,'\lead_locations.csv'));
-disp(lead_locations)
 
 % load in the geometry files of atria, ventricles and thorax
+%thorax
+[thorax_vertices, thorax_triangles] = loadtri(append(path,'\',model_file_name,'\thorax.tri'));
+writematrix(thorax_vertices,append('ECGI_SimData',export_filename,name,'\thorax_vertices.csv'));
+writematrix(thorax_triangles,append('ECGI_SimData',export_filename,name,'\thorax_triangles.csv'));
+%right blood cavity
+[rcav_vertices, rcav_triangles] = loadtri(append(path,'\',model_file_name,'\rcav.tri'));
+writematrix(rcav_vertices,append('ECGI_SimData',export_filename,name,'\rcav_vertices.csv'));
+writematrix(rcav_triangles,append('ECGI_SimData',export_filename,name,'\rcav_triangles.csv'));
+%left blood cavity
+[lcav_vertices, lcav_triangles] = loadtri(append(path,'\',model_file_name,'\lcav.tri'));
+writematrix(lcav_vertices,append('ECGI_SimData',export_filename,name,'\lcav_vertices.csv'));
+writematrix(lcav_triangles,append('ECGI_SimData',export_filename,name,'\lcav_triangles.csv'));
 
 % TMP = transmembrane potential, electrical potential on heart
 % atria
-if exist(append(path,ventricular_beats_filename),'dir') 
-    atrial_beats = loadmat(append(path,atrial_beats_filename,beat_filename));
-    disp(atrial_beats)
+if exist(append(path,'\',ventricular_beats_filename),'dir') 
+    atrial_beats = loadmat(append(path,'\',atrial_beats_filename,beat_filename));
+    writematrix(atrial_beats,append('ECGI_SimData',export_filename,name,'\atrial_beats.csv'));
 end
 
 % ventricles
-if exist(append(path,atrial_beats_filename),'dir') 
-    ventricular_beats = loadmat(append(path,ventricular_beats_filename,beat_filename))
-    disp(ventricular_beats)
+if exist(append(path,'\',atrial_beats_filename),'dir') 
+    ventricular_beats = loadmat(append(path,'\',ventricular_beats_filename,beat_filename))
+    writematrix(ventricular_beats,append('ECGI_SimData',export_filename,name,'\ventricular_beats.csv'));
 end
 
+% what is blood cavity?
 
